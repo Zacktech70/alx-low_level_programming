a176b892263fde509ff6a7337ac4d5f8a7b5eac3
Isaac Ngari <isaacngare730@gmail.com>
2023-02-16 12:05:10 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - Prints the size of various types based on 
+ * main - Prints the size of various types based on
  * the computer it is compiled and run on..
  * Return: Always 0.
  */
