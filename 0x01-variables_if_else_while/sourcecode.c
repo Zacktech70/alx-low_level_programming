cfe1d5dba500dde41222773e40634a21861f573d
Isaac Ngari <isaacngare730@gmail.com>
2023-02-20 11:27:32 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/sourcecode.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Prints if number is positive,
+ * Rturn: Always (Success)
+ *
+ */
+
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	return (0);
+}
