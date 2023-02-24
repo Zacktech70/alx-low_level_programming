be0759db1be00b9f0f09172d6eccc1aaf1996873
Isaac Ngari <isaacngare730@gmail.com>
2023-02-24 10:42:45 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,7 @@
  * Return: Nothing!
  */
 
+
 void print_number(int n)
 
 {
@@ -16,7 +17,7 @@ void print_number(int n)
 	{
 	n *= -1;
 	k = n;
-	_putchar('_');
+	_putchar('-');
 	}
 	k /= 10;
 	if (k != 0)
