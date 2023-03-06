9ae589db923d8c271a46b50778260a551f02e057
Zacktech <isaacngare730@gmail.com>
2023-03-06 20:00:32 +0300

    2. strchr - 101-crackme_password

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
  * _strchr - Entry point
  * @s: input
  * @c: input
  * Return: Always 0 (Success)
  */
-
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s + 1);
+			return (&s[i]);
 	}
-
-	return (NULL);
+	return (0);
 }
 		
